2459d2b457012b42215890e9403cb237361f2ab4
Gavin Watson <gavset99@gmail.com>
2019-04-06 14:36:50 -0400

    Initial app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/message', (req, res) => {
+    console.log('Message requested');
+
+    res.send({message: 'Message from backend'});
+});
+
+app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
